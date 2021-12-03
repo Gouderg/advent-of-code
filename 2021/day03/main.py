9b87c4b779a468bdd733bc43357853696857ee64
Gouderg <victor.illien974@gmail.com>
2021-12-03 15:05:52 +0100

    Ptite opti 3

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -11,9 +11,7 @@ with open('input.txt', 'r') as file:
 epsilon, gamma = "", ""
 
 for j in range(0, len(data[0])):
-    temp = []
-    for i in range(0, len(data)):
-        temp.append(data[i][j])
+    temp = [data[i][j] for i in range(0, len(data))]
     c = Counter(temp)
     gamma += max(c, key=lambda key: c[key])
     epsilon += min(c, key=lambda key: c[key])
@@ -23,46 +21,37 @@ print('Part 1:', int(gamma, 2) * int(epsilon, 2))
 
 # Part 2.
 
-# oxy. 
-oxy = data.copy()
+oxy, co2 = data.copy(), data.copy()
 index = 0
-while len(oxy) > 1:
 
-    # Find the count of bits in each column.
-    temp = []
-    for i in range(0, len(oxy)):
-        temp.append(oxy[i][index])
-    c = Counter(temp)
+while len(oxy) > 1 or len(co2) > 1:
 
-    # Keep 1 if same distribution else the one with the max of occurrence.
-    catMax = '1' if c[max(c, key=lambda key: c[key])] == c[min(c, key=lambda key: c[key])] else max(c, key=lambda key: c[key])
+    if len(oxy) > 1:
 
-    # Delete the wrong one.
-    for i in range(len(oxy)-1, -1, -1):
-        if oxy[i][index] != catMax:
-            oxy.pop(i)
+        # Find the count of bits in each column.
+        temp = [oxy[i][index] for i in range(0, len(oxy))]
+        c = Counter(temp)
 
-    index += 1
+        # Keep 1 if same distribution else the one with the max of occurrence.
+        catMax = '1' if c[max(c, key=lambda key: c[key])] == c[min(c, key=lambda key: c[key])] else max(c, key=lambda key: c[key])
+        
+        # Delete the wrong one.
+        for i in range(len(oxy)-1, -1, -1):
+            if oxy[i][index] != catMax:
+                oxy.pop(i)
+    
+    if len(co2) > 1:
+        # Find the count of bits in each column.
+        temp = [co2[i][index] for i in range(0, len(co2))]
+        c = Counter(temp)
 
+        # Keep 1 if same distribution else the one with the max of occurrence.
+        catMin = '0' if c[max(c, key=lambda key: c[key])] == c[min(c, key=lambda key: c[key])] else min(c, key=lambda key: c[key])
 
-# co2. 
-co2 = data.copy()
-index = 0
-while len(co2) > 1:
-
-    # Find the count of bits in each column.
-    temp = []
-    for i in range(0, len(co2)):
-        temp.append(co2[i][index])
-    c = Counter(temp)
-
-    # Keep 1 if same distribution else the one with the max of occurrence.
-    catMin = '0' if c[max(c, key=lambda key: c[key])] == c[min(c, key=lambda key: c[key])] else min(c, key=lambda key: c[key])
-
-    # Delete the wrong one.
-    for i in range(len(co2)-1, -1, -1):
-        if co2[i][index] != catMin:
-            co2.pop(i)
+        # Delete the wrong one.
+        for i in range(len(co2)-1, -1, -1):
+            if co2[i][index] != catMin:
+                co2.pop(i)
 
     index += 1
 
