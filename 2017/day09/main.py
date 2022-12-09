b50f9e4924aec80384e876d218a07d2560be23b7
Gouderg <victor.illien974@gmail.com>
2022-12-09 17:36:27 +0100

    2017: 9

--- 2017/day09/main.py
+++ 2017/day09/main.py
@@ -1,31 +1,35 @@
 
 # Extract data.
 data = []
-with open('input2.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     for row in file:
-        data.append(row.replace('\n', '').replace(',',''))
+        data.append(row.replace('\n', ''))
 
-# Part 1.
+# Part 1 & 2.
+for row in data:
+    ouverture, score, garbage, count = 0, 0, 0, 0
+    needPass = False 
+    for l in row:
+        if needPass:
+            needPass = False
+            continue
 
-for instruc in data:
-    ouverture, score = 0, 0
-    isOkey = True
-    isChapeau = False
-    for letter in instruc:
-        if not(isOkey) and letter == '>':
-            # ouverture -= 1
-            isOkey = False
-        elif not(isOkey):
-            isOkey = True
+        if l == '!' and garbage:
+            needPass = True
         
-        elif letter == '!':
-            isOkey = False
-        elif letter == '{':
+        if l == '{' and garbage == 0:
             ouverture += 1
-        elif letter == '}':
+        elif l == '}' and garbage == 0:
             score += ouverture
             ouverture -= 1
-        elif letter == '<':
-            isChapeau = True
 
+        if garbage and not(needPass) and l != '>':
+            count += 1
+
+        if l == '<':
+            garbage = 1
+        elif l == '>':
+            garbage = 0
+        
     print('Part 1:', score)
+    print('Part 2:', count)
