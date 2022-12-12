f047eb3ccae10cfaf8f6987063383df4fba786d4
Gouderg <victor.illien974@gmail.com>
2022-12-12 11:23:26 +0100

    2022: Day 11

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -1,31 +1,11 @@
 # Extract data.
 data = []
-with open("input.txt", "r") as file:
+with open("input2.txt", "r") as file:
     for row in file:
         data.append(list(row.replace('\n', '')))
 
 
 def recursion(i, j, value, already, score, best_score):
-    if value >= ord('y') and ((i-1 >= 0 and data[i-1][j] == 'E') or (i+1 < len(data) and data[i+1][j] == 'E') or (j-1 >= 0  and data[i][j-1] == 'E') or (j+1 < len(data[0]) and data[i][j+1] == 'E')):
-        # print(best_score, value, data[i][j], i, j, ord('y'), value)
-        best_score.append(score)
-        return best_score
-    
-    # Right.
-    if j+1 < len(data[0]) and [i, j+1] not in already and ord(data[i][j+1]) <= value+1:
-        best_score = recursion(i, j+1, ord(data[i][j+1]), already + [[i, j+1]], score + 1, best_score)
-
-    # Up.
-    if i-1 >= 0 and [i-1, j] not in already and ord(data[i-1][j]) <= value+1:
-        best_score = recursion(i-1, j, ord(data[i-1][j]), already + [[i-1, j]], score + 1, best_score)
-    
-    # Down.
-    if i+1 < len(data) and [i+1, j] not in already and ord(data[i+1][j]) <= value+1:
-        best_score = recursion(i+1, j, ord(data[i+1][j]), already + [[i+1, j]], score + 1, best_score)
-
-    # Left.
-    if j-1 >= 0 and [i, j-1] not in already and ord(data[i][j-1]) <= value+1:
-        best_score = recursion(i, j-1, ord(data[i][j-1]), already + [[i, j-1]], score + 1, best_score)
 
 
     return best_score
@@ -40,10 +20,18 @@ for i in range(0, len(data)):
             break
     if isOkey: break
 
-score = recursion(start[0], start[1], ord('a'), [start], 1, [])
+# Get the stop position.
+stop, isOkey = [], False
+for i in range(0, len(data)):
+    for j in range(0, len(data[i])):
+        if data[i][j] == 'E':
+            stop, isOkey = [i, j], True
+            break
+    if isOkey: break
 
 
-print("Part 1:", min(score))
+
+print("Part 1:", start, stop)
 
 # Part 2.
 print("Part 2:")
