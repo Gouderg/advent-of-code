285c7435ac3b7cc7df4f67b814f1a837a43e1bca
Gouderg <victor.illien974@gmail.com>
2022-12-14 19:07:12 +0100

    2017 day11

--- 2017/day11/main.py
+++ 2017/day11/main.py
@@ -1,4 +1,4 @@
-# Big love to : https://www.redblobgames.com/grids/hexagons/
+# Big love to : https://www.redblobgames.com/grids/hexagons/ and https://stackoverflow.com/questions/15919783/distance-between-2-hexagons-on-hexagon-grid
 
 # Extract data.
 data = []
@@ -6,31 +6,51 @@ with open("input.txt", "r") as file:
     for row in file:
         data = row.replace("\n", "").split(",")
 
-print(data)
 
-i, j = 50, 50
-for elt in data:
-    if elt == "nw":
-        i -= 1
+def dist(p1, p2):
+    y1, x1 = p1
+    y2, x2 = p2
+    du = x2 - x1
+    dv = (y2 + x2 // 2) - (y1 + x1 // 2)
+    return max(abs(du), abs(dv)) if ((du >= 0 and dv >= 0) or (du < 0 and dv < 0)) else abs(du) + abs(dv)
+
+def update(i, j, direction):
+
+    if direction == "nw":
         j -= 1
-    elif elt == "n":
+        i = i if j % 2 == 0 else i-1
+    elif direction == "n":
         i -= 1
-    elif elt == "ne":
-        i -= 1
+    elif direction == "ne":
+        i = i if j % 2 == 0 else i-1
         j += 1
-    elif elt == "sw":
-        pass
-    elif elt == "s":
-        pass
-    elif elt == "se":
-        pass
+    elif direction == "sw":
+        j -= 1
+        i = i if j % 2 == 0 else i+1
+    elif direction == "s":
+        i += 1
+    elif direction == "se":
+        j += 1
+        i = i if j % 2 == 0 else i+1
+    return i, j
+    
+# Part 1.
+i_init, j_init = 50, 50
+i, j = i_init, j_init
 
-'''
-Even q
-    1,0
-0,0     2,0
-    1,1 
-0,1     2,1
-    1,2
-'''
+for elt in data:
+    i, j = update(i, j, elt)
 
+print("Part 1:", dist((i_init, j_init), (i,j)))
+
+# Part 2.
+i_init, j_init = 0, 0
+i, j = i_init, j_init
+best_dist = 0
+for step, elt in enumerate(data):
+    i, j = update(i, j, elt)
+    current_dist = dist((i_init, j_init), (i,j))
+    if current_dist > best_dist:
+        best_dist = current_dist
+
+print("Part 2:", best_dist)
