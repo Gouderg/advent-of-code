0db74b19b3987267f75a9061e3f626f4a31bd223
Gouderg <victor.illien974@gmail.com>
2022-12-15 16:31:29 +0100

    2015: day15

--- 2022/day15/main.py
+++ 2022/day15/main.py
@@ -13,69 +13,48 @@ with open("input.txt", "r") as file:
 def dist(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-def n_impaire(n):
-    nb = 1
-    for _ in range(n):
-        nb += 2
-    return nb
-
 def getRow(y_to_find):
     already = []
-    known = 0
     for xs, ys, xb, yb in beacons:
-        if ys == y_to_find and [xs, xs] not in already:
-            already.append([xs, xs])
-            known += 1
-        if yb == y_to_find and [xb, xb] not in already:
-            already.append([xb, xb])
-            known += 1
 
-        distance = dist((xs, ys), (xb, yb))
-
+        distance = dist((xs,ys),(xb,yb))
         if (ys <= y_to_find and y_to_find <= ys + distance) or (ys >= y_to_find and y_to_find >= ys - distance):
-            n_val = n_impaire(distance - abs(ys-y_to_find))
-            already.append([xs-n_val//2, xs+n_val//2])
+            n_val = distance - abs(ys-y_to_find)
+            already.append([xs-n_val, xs+n_val])
 
 
     already = sorted(already)
+
     queu = []
     for l, h in already:
         if not queu:
             queu.append([l, h])
             continue
 
-        ql, qh = queu[-1]
+        _ , qh = queu[-1]
 
-        if l > qh + 1:
+        if l >= qh + 1:
             queu.append([l, h])
             continue
         
         queu[-1][1] = max(qh, h)
 
-    return queu, known
+    return queu
 
 # Part 1.
-
-y_to_find = 2000000
-y_to_find = 10
-     
-part1, known = getRow(y_to_find)       
-
-print("Part 1:", sum([abs(y - x) for x, y in part1]) - known + 1)
+print("Part 1:", sum([abs(y - x) for x, y in getRow(2000000)]) )
 
 # Part 2.
 max_range = 4000000
-
 index = 0
-while index < max_range:
-    v1, _ = getRow(index)
-
+while index <= max_range:
     x = 0
-    for l, h in v1:
+    for l, h in getRow(index):
         if x < l:
             print("Part 2:", x*max_range+index)
             exit(0)
+
         x = max(x, h+1)
-        if x > max_range: break
+        if x >= max_range: break
 
     index += 1
