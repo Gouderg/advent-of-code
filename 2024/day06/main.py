d13568d4312046bf6211c31b47881e0710e7a217
Gouderg <victor.illien974@gmail.com>
2024-12-06 10:08:30 +0400

    2024: day06

--- 2024/day06/main.py
+++ 2024/day06/main.py
@@ -6,6 +6,7 @@ with open("input.txt", "r") as file:
         row = row.replace("\n", "")
         data.append(list(row))
 
+
 def get_guard_pos():
     for i in range(0, len(data)):
         for j in range(0, len(data[0])):
@@ -58,53 +59,52 @@ print("Part 1:", len(distinct))
 # Part 2.
 
 cpt_stuck = 0
-for i in range(0, len(data)):
-    for j in range(0, len(data[0])):
+for i, j in distinct:
 
-        if i == initial_x and j == initial_y: continue
-        if data[i][j] != ".": continue
+    if i == initial_x and j == initial_y: continue
+    if data[i][j] != ".": continue
 
-        data[i][j] = "O"
-        max_loop, dir = 0, 0
-        p_x, p_y = initial_x, initial_y
-        isEscape = False
-        
-        while True:
+    data[i][j] = "O"
+    max_loop, dir = 0, 0
+    p_x, p_y = initial_x, initial_y
+    isEscape = False
+    
+    while True:
 
-            if max_loop > 10000: break
-            max_loop += 1
+        if max_loop > 10000: break
+        max_loop += 1
 
-            if dir == 0 and p_x-1 < 0 or dir == 1 and p_y+1 >= len(data[0]) or dir == 2 and p_x+1 >= len(data) or dir == 3 and p_y-1 < 0:
-                isEscape = True
-                break
+        if dir == 0 and p_x-1 < 0 or dir == 1 and p_y+1 >= len(data[0]) or dir == 2 and p_x+1 >= len(data) or dir == 3 and p_y-1 < 0:
+            isEscape = True
+            break
 
-            if dir == 0:
-                if data[p_x-1][p_y] == ".":
-                    p_x -= 1
-                else:
-                    dir = (dir + 1) % 4
+        if dir == 0:
+            if data[p_x-1][p_y] == ".":
+                p_x -= 1
+            else:
+                dir = (dir + 1) % 4
 
-            elif dir == 1:
-                if data[p_x][p_y+1] == ".":
-                    p_y += 1
-                else:
-                    dir = (dir + 1) % 4
+        elif dir == 1:
+            if data[p_x][p_y+1] == ".":
+                p_y += 1
+            else:
+                dir = (dir + 1) % 4
 
-            elif dir == 2:
-                if data[p_x+1][p_y] == ".":
-                    p_x += 1
-                else:
-                    dir = (dir + 1) % 4
+        elif dir == 2:
+            if data[p_x+1][p_y] == ".":
+                p_x += 1
+            else:
+                dir = (dir + 1) % 4
 
-            elif dir == 3:
-                if data[p_x][p_y-1] == ".":
-                    p_y -= 1
-                else:
-                    dir = (dir + 1) % 4
+        elif dir == 3:
+            if data[p_x][p_y-1] == ".":
+                p_y -= 1
+            else:
+                dir = (dir + 1) % 4
 
-        data[i][j] = "."
+    data[i][j] = "."
 
-        if not isEscape:
-            cpt_stuck += 1
+    if not isEscape:
+        cpt_stuck += 1
 
 print("Part 2:", cpt_stuck)
